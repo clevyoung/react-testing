671a84cae2855c29f14a9f01009fdb43f71f15df
clevyoung <clevyoung@gmail.com>
2021-09-24 11:01:37 +0900

    Disable order button is no scoops ordered

--- src/pages/sundaes-on-demand/entry/tests/OrderEntry.test.jsx
+++ src/pages/sundaes-on-demand/entry/tests/OrderEntry.test.jsx
@@ -6,6 +6,7 @@ import {
 import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../../mocks/server';
+import userEvent from '@testing-library/user-event';
 
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
@@ -28,3 +29,24 @@ test('handles error for scoops and toppings routes', async () => {
 });
 
 // test.skip()
+test('disable order button if there are no scoops ordered', async () => {
+  // if you're using typescript or something else that enforces props, then you'll need to use a jest mock function as prop for setOrderPhase
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  // order button should be disabled at first, even before options load
+  let orderButton = screen.getByRole('button', { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+  expect(orderButton).toBeEnabled();
+
+  // expect button to be disabled again after removing scoop
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '0');
+  expect(orderButton).toBeDisabled();
+});
