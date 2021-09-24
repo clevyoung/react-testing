e5308edfba60dd56885dee37fc1b05c551525d54
clevyoung <clevyoung@gmail.com>
2021-09-24 13:52:51 +0900

    No scoops subtotal update for invalid scoop count

--- src/pages/sundaes-on-demand/entry/ScoopOption.jsx
+++ src/pages/sundaes-on-demand/entry/ScoopOption.jsx
@@ -8,15 +8,18 @@ const ScoopOption = ({ name, imagePath, updateItemCount }) => {
 
   const handleChange = (event) => {
     const currentValue = event.target.value;
-    updateItemCount(name, currentValue);
 
     const currentValueFloat = parseFloat(currentValue);
 
-    setIsValid(
+    const valueIsValid =
       currentValueFloat >= 0 &&
-        currentValueFloat <= 10 &&
-        Math.floor(currentValueFloat) === currentValueFloat
-    );
+      currentValueFloat <= 10 &&
+      Math.floor(currentValueFloat) === currentValueFloat;
+
+    console.log(valueIsValid);
+    setIsValid(valueIsValid);
+
+    if (valueIsValid) updateItemCount(name, currentValue);
   };
 
   return (
